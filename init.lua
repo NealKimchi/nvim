48183dde75febe25f50dd8cac83bef2a28c5c571
NealKimchi <neal.w.kimchi@gmail.com>
2024-05-14 22:57:19 -0700

    Pushing treesitter changes again

--- init.lua
+++ init.lua
@@ -1,1 +1,8 @@
 require('neal.options')
+require('neal.keymaps')
+require('neal.plugins')
+require('neal.cmp')
+require('neal.lsp')
+require('neal.telescope')
+require('neal.treesitter')
+vim.cmd "colorscheme tokyonight"
